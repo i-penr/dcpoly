821e5b123ccada270d356b52a1c404cfa77d3b3b
Ismael Penalva <ismaelpenalvarodriguez@gmail.com>
2023-09-16 21:37:14 +0200

    Fixed ping cmd

--- src/commands/basic/ping.ts
+++ src/commands/basic/ping.ts
@@ -1,12 +1,12 @@
 import { CommandInteraction, SlashCommandBuilder} from "discord.js";
-import Command from '../../models/interfaces/Command'
+import Command from '../../models/interfaces/Command';
 
 const command: Command = {
     data: new SlashCommandBuilder()
             .setName('ping')
             .setDescription('Shows bot\'s latency in milliseconds'),
     async execute(interaction: CommandInteraction) {
-        await interaction.reply(`Pong!\nLatency: ${interaction.client.ws.ping} ms`);
+        await interaction.reply(`Pong!\nLatency: ${Date.now() - interaction.createdTimestamp} ms`);
     },
 }
 
